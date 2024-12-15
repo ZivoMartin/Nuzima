3cc56a3459a3078cef78b91f0bc7f8b7585c4530
martin <martin.zivojinovic@etu.u-bordeaux.fr>
2024-12-15 21:17:22 +0100

    Some command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 mod assembler;
 mod macros;
-use std::{fs::File, io::Read, process::exit};
+use std::{env::args, fs::File, io::Read, process::exit};
 
 use assembler::assemble;
 
 fn main() {
-    let mut f = File::open("./exemples/test.nzm").unwrap();
+    let mut f = File::open(args().nth(1).unwrap_or(String::from("./exemples/test.nzm"))).unwrap();
     let mut content = String::new();
     f.read_to_string(&mut content).unwrap();
     assemble(
         File::options()
             .write(true)
             .create(true)
-            .open("./a.out")
+            .open(args().nth(2).unwrap_or(String::from("a.out")))
             .expect("Failed to create a.out file"),
         &content,
     )
